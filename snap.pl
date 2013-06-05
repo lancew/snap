1c5ef18a3e7913a619c9f79faae5caeae29bf721
vagrant <vagrant@precise32.(none)>
2013-06-05 17:38:54 +0000

    70 minute... darn should not have used the Games::Cards lib as it slowed me down

--- snap.pl
+++ snap.pl
@@ -4,6 +4,18 @@ use warnings;
 use v5.10;
 
 use Games::Cards;
+use Player;
+
 my $Snap = new Games::Cards::Game;
 my $Deck = new Games::Cards::Deck( $Snap, "Deck" );
+$Deck->shuffle;
 
+my $player1 = Player->new;
+my $player2 = Player->new;
+
+#$player1->hand(new Games::Cards::Hand "Player1");
+
+
+$Deck->give_cards($hand1, 25);
+
+say $hand1;
